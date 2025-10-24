f175f13309f60268aee8e513dc1c37a6ad70472b
Harliton Martins <antonio.harliton@tce.am.gov.br>
2025-10-24 11:28:47 -0400

    correção dos DTOs

--- src/main/java/com/harliton/refeicoes_api/dto/CategoriaDTO.java
+++ src/main/java/com/harliton/refeicoes_api/dto/CategoriaDTO.java
@@ -2,10 +2,12 @@ package com.harliton.refeicoes_api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // DTO simples para representar a Categoria em listas
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoriaDTO {
      private Long id;
      private String titulo;
--- src/main/java/com/harliton/refeicoes_api/dto/ReceitaDTO.java
+++ src/main/java/com/harliton/refeicoes_api/dto/ReceitaDTO.java
@@ -1,10 +1,16 @@
 package com.harliton.refeicoes_api.dto;
 
-import com.harliton.refeicoes_api.enumeration.Dificuldade;
-import lombok.Data;
 import java.util.List;
 
+import com.harliton.refeicoes_api.enumeration.Dificuldade;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceitaDTO {
     private Long id;
     private String titulo;
@@ -14,5 +20,5 @@ public class ReceitaDTO {
     private Dificuldade dificuldade;
     private List<String> ingredientes;
     private List<String> passos;
-    private CategoriaDTO categoria; 
+    private CategoriaDTO categoria;
 }
